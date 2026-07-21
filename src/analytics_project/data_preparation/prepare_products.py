0c6566ed32f49641e9d6ea413e8be19c8a31e592
nwmgraspr <rmblamah@gmail.com>
2026-07-21 10:55:07 -0500

    Add debug print statements for directory paths
    Added print statements to display the current file and directory paths.

--- src/analytics_project/data_preparation/prepare_products.py
+++ src/analytics_project/data_preparation/prepare_products.py
@@ -41,7 +41,9 @@ PROJECT_ROOT: pathlib.Path = SCRIPTS_DATA_PREP_DIR.parents[2]
 DATA_DIR: pathlib.Path = PROJECT_ROOT / "data"
 RAW_DATA_DIR: pathlib.Path = DATA_DIR / "raw"
 PREPARED_DATA_DIR: pathlib.Path = DATA_DIR / "prepared"
-
+print("RUNNING FILE:", __file__)
+print("PROJECT_ROOT:", PROJECT_ROOT)
+print("RAW_DATA_DIR:", RAW_DATA_DIR)
 
 # Ensure the directories exist or create them
 DATA_DIR.mkdir(exist_ok=True)
